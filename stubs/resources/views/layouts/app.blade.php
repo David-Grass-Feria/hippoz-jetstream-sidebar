8171af0112bd5080fa54740dea41712b9018707a
Extrem16 <banshee1811@gmail.com>
2022-10-20 19:41:46 +0200

    1.1

--- stubs/resources/views/layouts/app.blade.php
+++ stubs/resources/views/layouts/app.blade.php
@@ -17,66 +17,18 @@
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
-        
-            <!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-<!--
-  This example requires updating your template:
+    
 
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
 <div x-data="{open:false}">
-    <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
+   
     <div x-show="open" class="relative z-40 md:hidden" role="dialog" aria-modal="true">
-      <!--
-        Off-canvas menu backdrop, show/hide based on off-canvas menu state.
-  
-        Entering: "transition-opacity ease-linear duration-300"
-          From: "opacity-0"
-          To: "opacity-100"
-        Leaving: "transition-opacity ease-linear duration-300"
-          From: "opacity-100"
-          To: "opacity-0"
-      -->
+     
       <div class="fixed inset-0 bg-gray-600 bg-opacity-75"></div>
   
       <div class="fixed inset-0 z-40 flex">
-        <!--
-          Off-canvas menu, show/hide based on off-canvas menu state.
-  
-          Entering: "transition ease-in-out duration-300 transform"
-            From: "-translate-x-full"
-            To: "translate-x-0"
-          Leaving: "transition ease-in-out duration-300 transform"
-            From: "translate-x-0"
-            To: "-translate-x-full"
-        -->
+      
         <div class="relative flex w-full max-w-xs flex-1 flex-col bg-gray-800 pt-5 pb-4">
-          <!--
-            Close button, show/hide based on off-canvas menu state.
-  
-            Entering: "ease-in-out duration-300"
-              From: "opacity-0"
-              To: "opacity-100"
-            Leaving: "ease-in-out duration-300"
-              From: "opacity-100"
-              To: "opacity-0"
-          -->
+         
           <div class="absolute top-0 right-0 -mr-12 pt-2">
             <button x-on:click="open = !open" type="button" class="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span class="sr-only">Close sidebar</span>
@@ -92,7 +44,7 @@
           </div>
           <div class="mt-5 h-0 flex-1 overflow-y-auto">
             <nav class="space-y-1 px-2">
-              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+            
              
               <x-hippoz-jetstream-sidebar::sidebar-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 <x-slot name="icon">
@@ -101,32 +53,27 @@
                 Dashboard
              </x-hippoz-jetstream-sidebar::sidebar-link>
        
-             <x-hippoz-jetstream-sidebar::sidebar-link href="{{ route('users.index') }}" :active="request()->routeIs('users.index')">
-                 <x-slot name="icon">
-                     M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z         
-                 </x-slot>
-                 Users
-              </x-hippoz-jetstream-sidebar::sidebar-link>
+            
             </nav>
           </div>
         </div>
   
         <div class="w-14 flex-shrink-0" aria-hidden="true">
-          <!-- Dummy element to force sidebar to shrink to fit close icon -->
+        
         </div>
       </div>
     </div>
   
     <!-- Static sidebar for desktop -->
     <div class="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
-      <!-- Sidebar component, swap this element with another sidebar if you like -->
+   
       <div class="flex min-h-0 flex-1 flex-col bg-gray-800">
         <div class="flex h-16 flex-shrink-0 items-center bg-gray-900 px-4">
             <x-jet-application-mark class="block h-9 w-auto" />
         </div>
         <div class="flex flex-1 flex-col overflow-y-auto">
           <nav class="flex-1 space-y-1 px-2 py-4">
-            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+         
            <x-hippoz-jetstream-sidebar::sidebar-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
            <x-slot name="icon">
             M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25
@@ -134,13 +81,6 @@
            Dashboard
         </x-hippoz-jetstream-sidebar::sidebar-link>
   
-        <x-hippoz-jetstream-sidebar::sidebar-link href="{{ route('users.index') }}" :active="request()->routeIs('users.index')">
-            <x-slot name="icon">
-                M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z         
-            </x-slot>
-            Users
-         </x-hippoz-jetstream-sidebar::sidebar-link>
-         
       
           
   
